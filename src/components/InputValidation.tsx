81747f84e504d4f095051169a878cfdce917070f
Tobias Lans <tobias.lans@gmail.com>
2024-05-13 13:35:38 +0200

    Added more XSS

--- src/components/InputValidation.tsx
+++ src/components/InputValidation.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
 const InputValidation = () => {
+  const [fromUrl, setFromUrl] = useState<string>("");
   const divRef = useRef<HTMLDivElement | null>(null);
   const inputRef = useRef<HTMLInputElement | null>(null);
   const blog = `<h3>Injektion</h3><u>Här kan det finnas jobbiga skript.<u>`;
@@ -12,6 +13,9 @@ const InputValidation = () => {
     if (divRef?.current) {
       divRef.current.innerHTML = "This could be XSS, or not ¯\\_(ツ)_/¯";
     }
+    const queryParams = new URLSearchParams(window.location.search);
+    const xss = queryParams.get("xss");
+    setFromUrl(xss ?? "");
   }, []);
   return (
     <div>
@@ -22,6 +26,8 @@ const InputValidation = () => {
       <div dangerouslySetInnerHTML={{ __html: blog }} />
 
       <div ref={divRef}></div>
+      <span>From url:</span>
+      <div id="fromUrl" dangerouslySetInnerHTML={{ __html: fromUrl }} />
     </div>
   );
 };
